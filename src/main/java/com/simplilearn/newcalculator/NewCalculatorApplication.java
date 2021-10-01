c2df22040f6854f82fdf8b018c248d3001374f59
student <prabzin@live.com>
2021-10-01 13:59:13 +0000

    add logic and test new

---
+++ src/main/java/com/simplilearn/newcalculator/NewCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.newcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/newcalculator/NewCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.newcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
